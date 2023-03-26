43fef31ee130182c1e3e9c510760d87b5b8bdf7d
Ritesh Kumar Singh <93989204+riteshsinghh-coder@users.noreply.github.com>
2023-03-26 16:09:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const images=[ "images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var count1=Math.random();
+var f1=Math.round(count1*10);
+var count2=Math.random();
+var f2=Math.round(count2*10);
+if(f1>5){
+    f1=f1-5;
+}
+if(f2>5){
+    f2=f2-5;
+}
+
+document.querySelector(".img1").setAttribute("src",images[f1]);
+document.querySelector(".img2").setAttribute("src",images[f2]);
+
+if(f1>f2){
+    document.querySelector(".winner").innerhtml="winner is player 1";
+}
+else{
+    document.querySelector(".winner").innerhtml="winner is player 2";
+}
+document.querySelector(".winner").innerhtml="winner is player 2";
